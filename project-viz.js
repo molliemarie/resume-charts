c44ab13b409f147dc7c1117b7873055a9ccf3dd5
Mollie Pettit <mollie.marie@gmail.com>
2017-08-27 15:14:01 -0500

    resizes svgs based on window width

--- project-viz.js
+++ project-viz.js
@@ -1,87 +1,104 @@
 
 var defaultOpacity = 0.7
 
-var projectViz = d3.select("#projects-viz").append("svg")
-  .attr("width", width + chartMargin.left + chartMargin.right)
-  .attr("height", projectHeight + chartMargin.top + chartMargin.bottom)
-  .append("g")
-  .attr("transform", "translate(" + chartMargin.left + "," + chartMargin.top + ")");
+function resize() {
 
-var skillDetail = d3.select("#project-text").append("svg")
-  .attr("width", width + chartMargin.left + chartMargin.right)
-  .attr("height", textHeight + textMargin.top + textMargin.bottom)
-  .append("g")
-  .attr("transform", "translate(" + textMargin.left + "," + textMargin.top + ")");
+	var chartMargin = {top: 0, right: 0, bottom: 20, left: 20}
+	textMargin = {top: 20, right: 20, bottom: 20, left: 20};
+
+	var window_width = window.innerWidth;
+	var width = window_width - chartMargin.left - chartMargin.right,
+	chartHeight = 500 - chartMargin.top - chartMargin.bottom,
+	textHeight = 50 - textMargin.top - textMargin.bottom,
+	projectHeight = 500 - chartMargin.top - chartMargin.bottom;
+
+	var projectViz = d3.select("#projects-viz").append("svg")
+	  .attr("width", width + chartMargin.left + chartMargin.right)
+	  .attr("height", projectHeight + chartMargin.top + chartMargin.bottom)
+	  .append("g")
+	  .attr("transform", "translate(" + chartMargin.left + "," + chartMargin.top + ")");
+
+	var skillDetail = d3.select("#project-text").append("svg")
+	  .attr("width", width + chartMargin.left + chartMargin.right)
+	  .attr("height", textHeight + textMargin.top + textMargin.bottom)
+	  .append("g")
+	  .attr("transform", "translate(" + textMargin.left + "," + textMargin.top + ")");
+
+	d3.csv("projects.csv", ready)
+
+	function ready(error, data) {
+	    if (error) throw error;
+
+	    console.log(data);
+
+		var colorScale = d3.scaleOrdinal(d3.schemeCategory10)
+			.domain([0, data.length - 1])
+
+	    var radius = 80
+	    buffer = 20
+	    circlesPerLine = Math.floor(width / (radius * 2 + buffer))
+
+	    var projectCircleGroup = projectViz.selectAll('.projectCircleGroup')
+	    	.data(data)
+	    	.enter().append('g')
+	    	.attr('class', 'projectCircleGroup')
+	    	.attr('transform', function(d,i) { 
+	    		var circleLocation = radius + i*(radius*2 + buffer)
+	    		if (circleLocation < width) {
+	    			var xVal =  radius + i*(radius*2 + buffer)
+	    			var yVal =  radius + buffer
+	    		} else {
+	    			var xVal = radius*2 + (i-circlesPerLine - 1)*(radius*2 + buffer)
+	    			var yVal = radius * 3 + buffer
+	    		}
+	    		return 'translate('+ xVal +',' + yVal +')'
+	    	})
+	    	.on('click', function(d) {
+
+	    		d3.select('#project-text')
+		            .attr('class', 'details-text')
+		            .text(d.description)
+
+		        d3.selectAll('circle')
+		        	.style('opacity', defaultOpacity)
+		        	.attr('r', radius)
+
+		       	d3.select(this)
+		       		.select('circle')
+		       		.transition()
+		       		.attr('r', radius + buffer/2)
+		       		.style('opacity', 1)
+
+		       	d3.selectAll('text')
+		       		.style('font-weight', 'normal')
+
+		       	d3.select(this)
+		       		.select('text')
+		       		.transition()
+		       		.style('font-weight', 'bold')
+	    	});
+
+	   projectCircleGroup.append('circle')
+	   	.attr('r', radius)
+	   	.style('fill', function(d, i) { return colorScale(i); })
+	   	.style('opacity', defaultOpacity)
+
+	  projectCircleGroup.append('text')
+	  	.text(function(d) {
+	  		return d.project
+	  	})
+	  	.style('text-anchor', 'middle')
+	}
+}
 
 
-d3.csv("projects.csv", function(error, data) {
-    if (error) throw error;
-
-    console.log(data);
-
-	var colorScale = d3.scaleOrdinal(d3.schemeCategory10)
-		.domain([0, data.length - 1])
-
-    var radius = 100
-    buffer = 20
-    circlesPerLine = Math.floor(width / (radius * 2 + buffer))
-
-    var projectCircleGroup = projectViz.selectAll('.projectCircleGroup')
-    	.data(data)
-    	.enter().append('g')
-    	.attr('class', 'projectCircleGroup')
-    	.attr('transform', function(d,i) { 
-    		var circleLocation = radius + i*(radius*2 + buffer)
-    		if (circleLocation < width) {
-    			var xVal =  radius + i*(radius*2 + buffer)
-    			var yVal =  radius + buffer
-    		} else {
-    			var xVal = radius*2 + (i-circlesPerLine)*(radius*2 + buffer)
-    			var yVal = radius * 3 + buffer
-    		}
-    		return 'translate('+ xVal +',' + yVal +')'
-    	})
-    	.on('click', function(d) {
-
-    		d3.select('#project-text')
-	            .attr('class', 'details-text')
-	            .text(d.description)
-
-	        d3.selectAll('circle')
-	        	.style('opacity', defaultOpacity)
-	        	.attr('r', radius)
-
-	       	d3.select(this)
-	       		.select('circle')
-	       		.transition()
-	       		.attr('r', radius + buffer/2)
-	       		.style('opacity', 1)
-
-	       	d3.selectAll('text')
-	       		.style('font-weight', 'normal')
-
-	       	d3.select(this)
-	       		.select('text')
-	       		.transition()
-	       		.style('font-weight', 'bold')
-    	});
-
-   projectCircleGroup.append('circle')
-   	.attr('r', radius)
-   	.style('fill', function(d, i) { return colorScale(i); })
-   	.style('opacity', defaultOpacity)
-
-  projectCircleGroup.append('text')
-  	.text(function(d) {
-  		return d.project
-  	})
-  	.style('text-anchor', 'middle')
-});
 
 
+// Draw for the first time to initialize.
+resize();
 
-
-
+// Redraw based on the new size whenever the browser window is resized.3
+window.addEventListener("resize", resize);
 
 
 
--- skillz-chart.js
+++ skillz-chart.js
@@ -2,40 +2,56 @@
 
 defaultBarColor = '#E7962A'
 
-var skillChart = d3.select("#skillz-chart").append("svg")
-  .attr("width", width + chartMargin.left + chartMargin.right)
-  .attr("height", chartHeight + chartMargin.top + chartMargin.bottom)
-  .append("g")
-  .attr("transform", "translate(" + chartMargin.left + "," + chartMargin.top + ")");
+function resize() {
 
-var skillDetail = d3.select("#details-text").append("svg")
-  .attr("width", width + chartMargin.left + chartMargin.right)
-  .attr("height", textHeight + textMargin.top + textMargin.bottom)
-  .append("g")
-  .attr("transform", "translate(" + textMargin.left + "," + textMargin.top + ")");
-    
-var xScale = d3.scaleLinear()
-  .range([0, width]);
+  d3.selectAll("svg").remove();
 
-var yScale = d3.scaleBand()
-  .range([chartHeight, 0])
-  .padding(0.3);
+  var chartMargin = {top: 0, right: 50, bottom: 20, left: 20}
+  textMargin = {top: 20, right: 20, bottom: 20, left: 20};
 
-var xAxis = d3.axisBottom(xScale)
-  .ticks(4)
-  .tickSize(-chartHeight);
+  var window_width = window.innerWidth;
+  var width = window_width - chartMargin.left - chartMargin.right,
+    chartHeight = 500 - chartMargin.top - chartMargin.bottom,
+    textHeight = 50 - textMargin.top - textMargin.bottom,
+    projectHeight = 500 - chartMargin.top - chartMargin.bottom;
 
-var yAxis = d3.axisLeft(yScale)
-  .tickSize(0)
-  .tickPadding(-5);
+  var skillChart = d3.select("#skillz-chart").append("svg")
+    .attr("width", width + chartMargin.left + chartMargin.right)
+    .attr("height", chartHeight + chartMargin.top + chartMargin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + chartMargin.left + "," + chartMargin.top + ")");
 
-d3.csv("skill_rating.csv", function(error, data) {
+  var skillDetail = d3.select("#details-text").append("svg")
+    .attr("width", width + chartMargin.left + chartMargin.right)
+    .attr("height", textHeight + textMargin.top + textMargin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + textMargin.left + "," + textMargin.top + ")");
+      
+  var xScale = d3.scaleLinear()
+    .range([0, width]);
+
+  var yScale = d3.scaleBand()
+    .range([chartHeight, 0])
+    .padding(0.3);
+
+  var xAxis = d3.axisBottom(xScale)
+    .ticks(4)
+    .tickSize(-chartHeight);
+
+  var yAxis = d3.axisLeft(yScale)
+    .tickSize(0)
+    .tickPadding(-5);
+
+  d3.csv("skill_rating.csv", ready)
+
+  function ready(error, data) {
+
     if (error) throw error;
 
     console.log(data)
-  
+
     data = data.reverse();
-  
+
     xScale.domain([0, 10])
     yScale.domain(data.map(function(d) { return d.skill; })).padding(0.1);
 
@@ -92,4 +108,11 @@ d3.csv("skill_rating.csv", function(error, data) {
         .attr("class", "y axis")
         .call(yAxis);
 
-});
+  }
+}
+
+// Draw for the first time to initialize.
+resize();
+
+// Redraw based on the new size whenever the browser window is resized.3
+window.addEventListener("resize", resize);
